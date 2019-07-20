452d9abc498cc0b9f92f16f878ad98b32ca9539b
Saoud Alfares <sfares85@gmail.com>
2019-07-20 16:26:58 +0300

    finished

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
@@ -6,6 +8,17 @@ class Restaurant(models.Model):
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
+    owner = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+
 
     def __str__(self):
     	return self.name
+
+class Item(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name   
